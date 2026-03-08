c70b2d24a2086a21b739e068ba64a3b8cdc437db
Oscar Muya <oscarmuyalx@gmail.com>
2026-03-08 10:38:42 +0300

    feat(tests): Added test bash file

--- src/main/java/com/oscar/app/App.java
+++ src/main/java/com/oscar/app/App.java
@@ -1,10 +1,6 @@
 package com.oscar.app;
 
-/**
- * Hello world!
- */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+  public static void main(String[] args) {
+  }
 }
