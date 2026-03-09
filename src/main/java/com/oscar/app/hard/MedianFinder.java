b0ec9bf059e7efcf4a161433011362b47f5b3bb9
Oscar Muya <oscarmuyalx@gmail.com>
2026-03-09 21:56:04 +0300

    feat(impl): Added a multiset implementation of the median finder

--- src/main/java/com/oscar/app/hard/MedianFinder.java
+++ src/main/java/com/oscar/app/hard/MedianFinder.java
@@ -2,6 +2,7 @@ package com.oscar.app.hard;
 
 import java.util.Collections;
 import java.util.PriorityQueue;
+import java.util.TreeMap;
 
 public class MedianFinder {
   private PriorityQueue<Integer> lowerHalf;
@@ -25,5 +26,46 @@ public class MedianFinder {
       return lowerHalf.peek();
     else
       return (lowerHalf.peek() + upperHalf.peek()) / 2.0;
+  }
+}
+
+class MedianFinderMultiSet {
+  private TreeMap<Integer, Integer> lowerHalf;
+  private TreeMap<Integer, Integer> upperHalf;
+  private int lowerSize = 0, upperSize = 0;
+
+  public MedianFinderMultiSet() {
+    lowerHalf = new TreeMap<>(Collections.reverseOrder());
+    upperHalf = new TreeMap<>();
+  }
+
+  public int removeFirst(TreeMap<Integer, Integer> multiset) {
+    int key = multiset.firstKey();
+    int cnt = multiset.get(key) - 1;
+    if (cnt == 0)
+      multiset.remove(key);
+    else
+      multiset.put(key, cnt);
+    return key;
+  }
+
+  public void addNum(int num) {
+    lowerHalf.put(num, lowerHalf.getOrDefault(num, 0) + 1);
+    int max = removeFirst(lowerHalf);
+    upperHalf.put(max, upperHalf.getOrDefault(max, 0) + 1);
+    upperSize++;
+
+    if (upperSize > lowerSize) {
+      int min = removeFirst(upperHalf);
+      lowerHalf.put(min, lowerHalf.getOrDefault(min, 0) + 1);
+      upperSize--;
+      lowerSize++;
+    }
+  }
+
+  public double findMedian() {
+    if (lowerSize > upperSize)
+      return lowerHalf.firstKey();
+    return (lowerHalf.firstKey() + upperHalf.firstKey()) / 2.0;
   }
 }
